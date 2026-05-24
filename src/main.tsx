76dfc6affd75a110d263c70e03658a22dd84947c
Jorge Mauadie <99563509+Jorgeniuus@users.noreply.github.com>
2026-05-24 11:42:51 -0300

    fix: replace BrowserRouter with HashRouter in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,8 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import './index.css'
-// import App from './App.tsx'
-
-// createRoot(document.getElementById('root')!).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-// )
-
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import {
-  BrowserRouter,
+  HashRouter,
   Routes,
   Route,
 } from 'react-router-dom';
@@ -28,7 +16,7 @@ ReactDOM.createRoot(
   document.getElementById('root')!
 ).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<App />} />
 
@@ -37,6 +25,6 @@ ReactDOM.createRoot(
           element={<Unsubscribe />}
         />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
